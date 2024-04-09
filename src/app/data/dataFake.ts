222605f7dd4c15289678bc070c573a99cfa93a56
Yan <yancarlosduartevelasco@gmail.com>
2024-04-09 11:31:43 -0300

    Criando outras páginas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,62 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: 'aaaaa',
+    title: 'Novo filme',
+    description: 'Descrição do filme',
+  },
+  {
+    id: 2,
+    photo: 'aaaaa',
+    title: 'Novo filme',
+    description: 'Descrição do filme',
+  },
+  {
+    id: 3,
+    photo: 'bbbbb',
+    title: 'Outro filme',
+    description: 'Descrição de outro filme',
+  },
+  {
+    id: 4,
+    photo: 'ccccc',
+    title: 'Mais um filme',
+    description: 'Descrição de mais um filme',
+  },
+  {
+    id: 5,
+    photo: 'ddddd',
+    title: 'Filme incrível',
+    description: 'Descrição de um filme incrível',
+  },
+  {
+    id: 6,
+    photo: 'eeeee',
+    title: 'Filme surpreendente',
+    description: 'Descrição de um filme surpreendente',
+  },
+  {
+    id: 7,
+    photo: 'fffff',
+    title: 'Filme emocionante',
+    description: 'Descrição de um filme emocionante',
+  },
+  {
+    id: 8,
+    photo: 'ggggg',
+    title: 'Filme divertido',
+    description: 'Descrição de um filme divertido',
+  },
+  {
+    id: 9,
+    photo: 'hhhhh',
+    title: 'Filme inspirador',
+    description: 'Descrição de um filme inspirador',
+  },
+  {
+    id: 10,
+    photo: 'iiiii',
+    title: 'Filme inovador',
+    description: 'Descrição de um filme inovador',
+  },
+];
